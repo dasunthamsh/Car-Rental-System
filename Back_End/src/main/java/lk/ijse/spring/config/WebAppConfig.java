c1ac5fbcb0896a2b3fa600ec507832056dcaa2f8
Dasun Thamash <tunathamash@gmail.com>
2023-10-23 23:39:59 +0530

    WebRootConfig added

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -2,5 +2,7 @@ package lk.ijse.spring.config;/*
     @author Dasun
 */
 
+
 public class WebAppConfig {
+
 }
--- Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -2,5 +2,23 @@ package lk.ijse.spring.config;/*
     @author Dasun
 */
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({JAPConfig.class})
+@ComponentScan(basePackages = "lk.ijse.spring.service")
 public class WebRootConfig {
+
+    public WebRootConfig(){
+        System.out.println("web root config");
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
