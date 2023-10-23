2947b3773bb9d7924279c2c7f785c041707cdc17
Dasun Thamash <tunathamash@gmail.com>
2023-10-23 23:27:36 +0530

    JPAConfig class added

--- Back_End/src/main/java/lk/ijse/spring/config/JAPConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/JAPConfig.java
@@ -2,5 +2,71 @@ package lk.ijse.spring.config;/*
     @author Dasun
 */
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
+@EnableTransactionManagement
+@PropertySource("classpath:properties.properties")
 public class JAPConfig {
+
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter vad){
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(ds);
+        factory.setJpaVendorAdapter(vad);
+        factory.setPackagesToScan(env.getRequiredProperty("pro.entity"));
+        return factory;
+    }
+
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dmcs = new DriverManagerDataSource();
+        dmcs.setUsername(env.getRequiredProperty("pro.username"));
+        dmcs.setPassword(env.getRequiredProperty("pro.password"));
+        dmcs.setDriverClassName(env.getRequiredProperty("pro.driver"));
+        dmcs.setUrl(env.getRequiredProperty("pro.url"));
+        return dmcs;
+    }
+
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setDatabasePlatform(env.getRequiredProperty("pro.dial"));
+        va.setShowSql(true);
+        return va;
+
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+        return new JpaTransactionManager(factory);
+    }
+
 }
