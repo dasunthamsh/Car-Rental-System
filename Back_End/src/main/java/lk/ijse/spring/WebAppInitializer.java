9d926a2188c5f0385f277d988fa0dcc897f7c24d
Dasun Thamash <tunathamash@gmail.com>
2023-10-23 23:04:11 +0530

    WebAppInitializer added

--- Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ Back_End/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -2,5 +2,23 @@ package lk.ijse.spring;/*
     @author Dasun
 */
 
-public class WebAppInitializer extends Abstrac{
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
