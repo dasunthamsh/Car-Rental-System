a52793007b46af68767f38f474369d3ba9f9d31c
Dasun Thamash <tunathamash@gmail.com>
2023-10-23 23:41:02 +0530

    WebAppConfig added

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -2,7 +2,13 @@ package lk.ijse.spring.config;/*
     @author Dasun
 */
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"lk.ijse.spring.controller","lk.ijse.spring.advisor"})
 public class WebAppConfig {
 
 }
