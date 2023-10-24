17c4489e6b96bf20c5e814627509bbb74902ba26
Dasun Thamash <tunathamash@gmail.com>
2023-10-24 10:18:22 +0530

    Customer entity class added

--- Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -2,5 +2,41 @@ package lk.ijse.spring.entity;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+
+    @Id
+    private String customerId;
+    private String name;
+    private String address;
+    private int contactNo;
+    private String email;
+    private String nicNo;
+    private String nicFrontImage;
+    private String nicBackImage;
+    private String licenceNo;
+    private String licenceImage;
+    private String userName;
+    private String password;
+    private String status;
+
+
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+    private List<Rent> rents = new ArrayList<>();
 }
