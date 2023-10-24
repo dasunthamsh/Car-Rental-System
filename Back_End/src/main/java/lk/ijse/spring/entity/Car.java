85bcdb2f7bfdd28f59ce2bb708abbb26767f815f
Dasun Thamash <tunathamash@gmail.com>
2023-10-24 10:09:12 +0530

    car entity class added

--- Back_End/src/main/java/lk/ijse/spring/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -2,5 +2,53 @@ package lk.ijse.spring.entity;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Car {
+    @Id
+
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int NoOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+
+    private String frontViewImage;
+    private String backView;
+    private String sideView;
+    private String internalView;
+
+    private double dailyRate;
+    private double monthlyRate;
+    private  double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private String status;
+
+    @OneToMany(mappedBy = "car" , cascade = CascadeType.ALL)
+    private List<Rent> rents = new ArrayList<>();
+
+    // maintenances
+
+
+
 }
