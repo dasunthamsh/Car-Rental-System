af4467480b1c2d15714099680433a256b9abd952
Dasun Thamash <tunathamash@gmail.com>
2023-10-24 09:44:57 +0530

    login entity class added

--- Back_End/src/main/java/lk/ijse/spring/entity/Login.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Login.java
@@ -2,5 +2,26 @@ package lk.ijse.spring.entity;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Login {
+
+    @Id
+    private String loginId;
+    private String role;
+    private String username;
+    private String password;
+
+
 }
