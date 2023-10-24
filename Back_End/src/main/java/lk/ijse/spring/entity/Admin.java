bc9c4d3593162bc78314ccd01dd327ed392c47f8
Dasun Thamash <tunathamash@gmail.com>
2023-10-24 10:48:03 +0530

    Admin entity class added

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -0,0 +1,29 @@
+package lk.ijse.spring.entity;/*
+    @author Dasun
+*/
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Admin {
+    @Id
+    private String adminId;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String username;
+    private String password;
+
+
+}
