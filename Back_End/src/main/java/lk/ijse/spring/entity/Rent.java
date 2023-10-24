ef9d91df9ae4fab654a0e3c7195cb5c69fa1c396
Dasun Thamash <tunathamash@gmail.com>
2023-10-24 10:42:47 +0530

    Rent entity class added

--- Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
@@ -2,5 +2,49 @@ package lk.ijse.spring.entity;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rent {
+
+    @Id
+    private String rentId;
+    private String date;
+    private String pickupDate;
+    private String returnDate;
+    private String pickUpVenue;
+    private String returnVenue;
+    private String lossDamageWaiver;
+    private String bankSlip;
+    private String status;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "licenceNo",referencedColumnName = "licenceNo")
+    private Driver driver;
+
+    @ManyToOne
+    @JoinColumn(name = "registrationNO" , referencedColumnName = "registrationNO")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId",referencedColumnName = "customerId")
+    private Customer customer;
+
+
+
+
 }
--- Back_End/src/main/java/lk/ijse/spring/entity/Vehicle.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.spring.entity;/*
-    @author Dasun
-*/
-
-public class Vehicle {
-}
