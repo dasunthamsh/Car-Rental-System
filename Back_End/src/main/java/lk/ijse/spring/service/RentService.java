ad8310cb023ccf22bb13c5981cf6d6037c01e0ed
Dasun Thamash <tunathamash@gmail.com>
2023-11-02 05:11:23 +0530

    rent reservation save front end method added

--- Back_End/src/main/java/lk/ijse/spring/service/RentService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/RentService.java
@@ -4,7 +4,11 @@ package lk.ijse.spring.service;/*
 
 import lk.ijse.spring.dto.RentDTO;
 
+import java.util.List;
+
 public interface RentService {
 
     void bookingCar(RentDTO dto);
+
+    List<RentDTO> rentRequest();
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.service.impl;/*
     @author Dasun
 */
 
+import lk.ijse.spring.dto.CarDTO;
 import lk.ijse.spring.dto.RentDTO;
 import lk.ijse.spring.entity.Car;
 import lk.ijse.spring.entity.Customer;
@@ -11,10 +12,12 @@ import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.repo.RentRepo;
 import lk.ijse.spring.service.RentService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -38,15 +41,18 @@ public class RentServiceImpl implements RentService {
         if (!rentRepo.existsById(dto.getRentId())){
             Rent rent = mapper.map(dto, Rent.class);
             rentRepo.save(rent);
-//            Car car = carRepo.getReferenceById(registrationNO);
-//            Customer customer = customerRepo.getReferenceById(customerId);
-//            dto.setCar(car);
-//            dto.setCustomer(customer);
-//            rentRepo.save(dto);
 
         }else {
             throw new RuntimeException("Booking was added Already");
         }
 
     }
+
+    @Override
+    public List<RentDTO> rentRequest() {
+        List<Rent> all = rentRepo.findAll();
+        return mapper.map(all,new TypeToken<List<RentDTO>>(){}.getType());
+    }
+
+
 }
