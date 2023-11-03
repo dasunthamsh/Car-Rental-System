305d37137cb7e8b1cc4cd9bd11b49b56f6c64a82
Dasun Thamash <tunathamash@gmail.com>
2023-11-03 20:57:08 +0530

    driver repo updated

--- Back_End/src/main/java/lk/ijse/spring/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/DriverRepo.java
@@ -6,4 +6,6 @@ import lk.ijse.spring.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DriverRepo extends JpaRepository<Driver,String> {
+
+    Driver findByLicenceNo(String licenceNo);
 }
